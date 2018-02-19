35d39c41bc637da2641e6bf45603e9fabbdd42b7
hazim1093 <hazim_malik@hotmail.com>
2018-02-19 18:21:47 +0500

    Remove space

--- 3.5.1/curator-config/create-snapshot-repository.sh
+++ 3.5.1/curator-config/create-snapshot-repository.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Create backup repository in ES
-BODY='{"type":"s3","settings":{"bucket":"BUCKET_NAME","region":"AWS_REGION", "base_path":"BASE_PATH"}}'
+BODY='{"type":"s3","settings":{"bucket":"BUCKET_NAME","region":"AWS_REGION","base_path":"BASE_PATH"}}'
 
 BODY=$(echo ${BODY} | sed s,BUCKET_NAME,${BUCKET_NAME},g)
 BODY=$(echo ${BODY} | sed s,AWS_REGION,${AWS_REGION},g)
