b90719242e2867031e686a6384f1ab45be7609b9
Hazim <hazim_malik@hotmail.com>
2018-07-02 12:47:42 +0500

    Add dry run

--- 3.5.1/curator-config/curate.sh
+++ 3.5.1/curator-config/curate.sh
@@ -5,6 +5,7 @@ if [ -n "${DO_SNAPSHOT}" ];
 then
     echo "$(date) - Creating Snapshot on repository: ${SNAPSHOT_NAME} for indices older than ${OLDER_THAN} ${TIME_UNIT}"
     /usr/bin/curator \
+        ${DRY_RUN} \
         --loglevel ${LOGLEVEL} \
         --logfile ${LOGFILE} \
         --host ${ELASTICSEARCH_HOST} \
@@ -22,6 +23,7 @@ if [ -n ${DO_DELETE} ];
 then
     echo "$(date) - Deleting indices older than ${OLDER_THAN} ${TIME_UNIT}"
     /usr/bin/curator \
+        ${DRY_RUN} \
         --loglevel ${LOGLEVEL} \
         --logfile ${LOGFILE} \
         --host ${ELASTICSEARCH_HOST} \
